15d9a0cff120fcddf4cd7f7b513636fb2047b65f
Brian <braal.vf@gmail.com>
2023-02-15 20:52:09 -0400

    Agregar más info al readme

---
+++ python_03.py
@@ -0,0 +1,14 @@
+nombre = input("¿Cuál es su nombre?\n")
+edad = int(input('¿Cuántos años tiene?\n'))
+ciudad = input('¿En qué ciudad vive?\n')
+estado_civil = input('¿Está casado?\n')
+numero_a = int(input('Ingrese un número entre 1-100\n'))
+numero_b = int(input('Ingrese un número entre 100-200\n'))
+division = numero_a/numero_b
+print(f'Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {division}')
+#Crear una copia de un repositorio en tu computadora: git clone <URL>
+#Sincronizar para obtener cambios más recientes: git pull
+#Agregar un archivo al área de stage: git add <ARCHIVO>
+#Crear un commit con los archivos del área de stage: git commit -m “Mensaje commit”
+#Crear un nuevo branch en nuestra computadora: git checkout -b <BRANCH>
+#Cambiar a un branch existente: git checkout <BRANCH>
